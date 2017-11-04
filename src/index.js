999f0a34a70783f881c4a3a4369c444a147e00e9
Marat Latypov <fender87marat@gmail.com>
2017-11-04 10:07:40 +0300

    git

--- src/index.js
+++ src/index.js
@@ -2,31 +2,33 @@ import readlineSync from 'readline-sync';
 import { car, cdr } from 'hexlet-pairs';
 
 
-const play = (x, acc, f) => {
+const play = (userName, acc, funcGame) => {
   if (acc === 3) {
-    return console.log(`Congratulations, ${x}!`);
+    return console.log(`Congratulations, ${userName}!`);
   }
-  const fu = f();
-  const zagad = car(fu);
-  console.log(`Question: ${zagad}`);
-  const answ = readlineSync.question('Your answer: ');
-  const trueAnsw = cdr(fu);
-  if (answ !== trueAnsw) {
-    return console.log(`'${answ}' is wrong answer ;(. Correct answer was '${trueAnsw}'.\nLet's try again, ${x}!`);
+  const content = funcGame();
+  const question = car(content);
+  console.log(`Question: ${question}`);
+  const userAnsw = readlineSync.question('Your answer: ');
+  const trueAnsw = cdr(content);
+  if (userAnsw !== trueAnsw) {
+    return console.log(`'${userAnsw}' is wrong answer ;(. Correct answer was '${trueAnsw}'.\nLet's try again, ${userName}!`);
   }
   console.log('Correct!');
-  return play(x, acc + 1, f);
+  return play(userName, acc + 1, funcGame);
 };
 
-export const brainGames = (x, fun) => {
+export const brainGames = (gameName, funGame) => {
   const greeting = 'Welcome to the Brain Games!';
-  const name = () => readlineSync.question('May I have your name? ');
-  if (x !== undefined) {
-    console.log(`${greeting}\n${x}\n`);
-    return play(name(), 0, fun);
+  const userName = () => readlineSync.question('May I have your name? ');
+  if (gameName !== undefined) {
+    console.log(`${greeting}\n${gameName}\n`);
+    const usName = userName();
+    console.log(`Hello, ${usName}!\n`);
+    return play(usName, 0, funGame);
   }
   console.log(`${greeting}\n`);
-  return console.log(`Hello, ${name()}!\n`);
+  return console.log(`Hello, ${userName()}!\n`);
 };
 
 export default play;
